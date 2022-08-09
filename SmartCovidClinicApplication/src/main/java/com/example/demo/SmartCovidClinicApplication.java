4eb9f89d5fc515d52140c25ebfc75947c8185ce1
Pradnya Khot <Pradnya Khot@DESKTOP-04QRJD4>
2022-08-09 12:53:54 +0530

    Smart Covid Clinic

---
+++ SmartCovidClinicApplication/src/main/java/com/example/demo/SmartCovidClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCovidClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCovidClinicApplication.class, args);
+	}
+
+}
